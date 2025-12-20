84b9c8391f12eec41e9a2011dedd01ff939f3a4a
deidarooochka <nastaryckova446@gmail.com>
2025-12-20 18:28:44 +0400

    итоговая

--- kursovaya/kursovaya.cpp
+++ kursovaya/kursovaya.cpp
@@ -9,11 +9,10 @@ using namespace std;
 void bubbleSort(vector<int>& arr) {
     int n = arr.size();
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1; j++) {
+        for (int j = 0; j < n - 1 - i; j++)
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
             }
-        }
     }
 }
 
